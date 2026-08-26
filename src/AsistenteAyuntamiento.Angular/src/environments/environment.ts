7e7751f7a013ec6da5d41c7a9f3c8efbf67d2d2c
TempooDev <a.bermudez.freelance@gmail.com>
2026-08-26 16:10:20 +0200

    feat: update Auth0 configuration in environment files for development

--- src/AsistenteAyuntamiento.Angular/src/environments/environment.ts
+++ src/AsistenteAyuntamiento.Angular/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   auth0: {
-    domain: 'TU_DOMINIO.auth0.com',
-    clientId: 'TU_CLIENT_ID',
-    audience: 'TU_AUDIENCE'
+    domain: 'dev-va85m1jw6xjmlb5z.eu.auth0.com',
+    clientId: 'ZgTUeFqHR4VHnqWtlvhc8P3vbCbb3XgH',
+    audience: 'https://api.asistente-ayuntamiento.local'
   },
   apiBaseUrl: ''
 };
