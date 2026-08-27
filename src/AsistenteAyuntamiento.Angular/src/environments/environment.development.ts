59f93eb390a5eeb848628892598322ba7b5c9a88
TempooDev <a.bermudez.freelance@gmail.com>
2026-08-27 16:38:34 +0200

    fix: set apiBaseUrl to use environment variable in both development and production

--- src/AsistenteAyuntamiento.Angular/src/environments/environment.development.ts
+++ src/AsistenteAyuntamiento.Angular/src/environments/environment.development.ts
@@ -5,5 +5,5 @@ export const environment = {
     clientId: process.env.NG_APP_AUTH0_CLIENT_ID || '',
     audience: process.env.NG_APP_AUTH0_AUDIENCE || ''
   },
-  apiBaseUrl: ''
+  apiBaseUrl: process.env.NG_APP_API_BASE_URL || ''
 };
