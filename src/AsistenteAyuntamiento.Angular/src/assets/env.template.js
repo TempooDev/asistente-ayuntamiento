a661d9236cee37b37bb611d3f8fe7f6d32982bde
TempooDev <a.bermudez.freelance@gmail.com>
2026-09-01 18:27:23 +0200

    fix: use standard uppercase variables for envsubst to ensure bash compatibility

--- src/AsistenteAyuntamiento.Angular/src/assets/env.template.js
+++ src/AsistenteAyuntamiento.Angular/src/assets/env.template.js
@@ -1,6 +1,6 @@
 (function(window) {
   window.__env = window.__env || {};
-  window.__env.auth0Domain = "${Auth0__Domain}";
-  window.__env.auth0ClientId = "${Auth0__ClientId}";
-  window.__env.auth0Audience = "${Auth0__Audience}";
+  window.__env.auth0Domain = "${AUTH0_DOMAIN}";
+  window.__env.auth0ClientId = "${AUTH0_CLIENT_ID}";
+  window.__env.auth0Audience = "${AUTH0_AUDIENCE}";
 })(this);
