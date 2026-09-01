a696f3756c3b2c2f32a3f2b19e5041570a4f49e7
TempooDev <a.bermudez.freelance@gmail.com>
2026-09-02 00:03:03 +0200

    fix(auth): use snapshot query params in login route to avoid race condition

--- src/AsistenteAyuntamiento.Angular/src/app/pages/login/login.ts
+++ src/AsistenteAyuntamiento.Angular/src/app/pages/login/login.ts
@@ -19,25 +19,24 @@ export class LoginComponent implements OnInit {
   public auth = inject(AuthService);
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      const invitation = params['invitation'];
-      const organization = params['organization'];
-      
-      const authParams: any = {};
-      
-      if (invitation) {
-        authParams.invitation = invitation;
-      }
-      
-      if (organization) {
-        authParams.organization = organization;
-        // Guardamos la org para futuros accesos directos
-        localStorage.setItem('last_org_id', organization);
-      }
-      
-      this.auth.loginWithRedirect({
-        authorizationParams: Object.keys(authParams).length > 0 ? authParams : undefined
-      });
+    const params = this.route.snapshot.queryParams;
+    const invitation = params['invitation'];
+    const organization = params['organization'];
+    
+    const authParams: any = {};
+    
+    if (invitation) {
+      authParams.invitation = invitation;
+    }
+    
+    if (organization) {
+      authParams.organization = organization;
+      // Guardamos la org para futuros accesos directos
+      localStorage.setItem('last_org_id', organization);
+    }
+    
+    this.auth.loginWithRedirect({
+      authorizationParams: Object.keys(authParams).length > 0 ? authParams : undefined
     });
   }
 }
