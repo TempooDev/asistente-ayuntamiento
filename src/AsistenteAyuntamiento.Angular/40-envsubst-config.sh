f7509a58d860b9beb9e1729bc2b5bfadc338af78
TempooDev <a.bermudez.freelance@gmail.com>
2026-09-01 18:49:18 +0200

    refactor: use native nginx templates for env.js injection

--- src/AsistenteAyuntamiento.Angular/40-envsubst-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Generate env.js from template using environment variables
-envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
--- src/AsistenteAyuntamiento.Angular/public/assets/env.template.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-  window.__env = window.__env || {};
-  window.__env.auth0Domain = "${AUTH0_DOMAIN}";
-  window.__env.auth0ClientId = "${AUTH0_CLIENT_ID}";
-  window.__env.auth0Audience = "${AUTH0_AUDIENCE}";
-})(this);
